5957e48588beefd166a9abca0602f424d2ce1174
agcjunior <antonio.jr@outlook.com>
2017-03-29 09:21:06 -0300

    mock-html

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,18 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl=getBaseUrl();
 
 export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(id){
+  return del(`users/${id}`);
+}
+
 function get(url){
-  return fetch(url).then(onSuccess,onError);
+  return fetch(baseUrl + url).then(onSuccess,onError);
 }
 
 function onSuccess(response){
@@ -14,4 +21,10 @@ function onSuccess(response){
 
 function onError(error){
   console.log(error);
+}
+
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method:'DELETE'
+  });
 }
